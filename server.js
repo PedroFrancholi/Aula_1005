3c3fc3a886267a85cc17f137ef3f1f6185f36132
PedroFrancholi <francholidearaujo@gmail.com>
2023-05-23 01:46:34 -0300

    Aula2205

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const productsRoutes = require("./routes/prodtucts")
+const productsRoutes = require("./routes/products")
+const usersRoutes = require("./routes/users")
 const healthRoutes = require("./routes/health")
 
 const server = express();
 server.use(express.json());
 
 server.use(productsRoutes.router);
+server.use(usersRoutes.router);
 server.use(healthRoutes.router);
 
 module.exports = server
